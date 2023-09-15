cfcd7f65b8d95eedd867532a164d009eef85d107
sabrerodriguez <sabre.rodriguez@kidstrong.com>
2023-09-15 11:30:00 -0700

    Added way for user to see their current time zone

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let cityElement = document.querySelector("#city-cards");
