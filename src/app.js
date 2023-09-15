40f00089f093265dd11cbae36de7b347b0f5270f
sabrerodriguez <sabre.rodriguez@kidstrong.com>
2023-09-15 12:00:53 -0700

    introduced function to stop and restart second refresh on current time

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
+function currentTime() {
+  let current = moment.tz.guess();
+  let cityName = current.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(current);
+  let cityElement = document.querySelector("#city-cards");
+
+  cityElement.innerHTML = `<div class="city-card">
+<div>
+  <h2 class="city">${cityName}</h2>
+  <p class="date">${cityTime.format("MMMM Do YYYY")}</p>
+</div>
+<div>
+  <h3 class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></h3>
+</div>
+</div>`;
+}
+
 function updateCity(event) {
+  clearInterval(refresh);
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
+    refresh = setInterval(currentTime, 1000);
     cityTimeZone = moment.tz.guess();
   }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
@@ -20,6 +41,8 @@ function updateCity(event) {
 </div>`;
 }
 
+currentTime();
+var refresh = setInterval(currentTime, 1000);
+
 let selectionElement = document.querySelector("#cities");
 selectionElement.addEventListener("change", updateCity);
-setInterval(updateCity, 1000);
